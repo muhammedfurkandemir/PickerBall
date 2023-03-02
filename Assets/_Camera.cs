9fff031ff9e353f83817de97789850f99d8e9ed2
Furkan Demir <furkan.vty0@gmail.com>
2023-03-03 02:25:21 +0300

    .

---
+++ Assets/_Camera.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _Camera : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    [SerializeField] private Vector3 target_Offset;
+  
+
+    private void LateUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, target.position + target_Offset, .125f);
+    }
+}
